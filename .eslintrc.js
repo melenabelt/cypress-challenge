e3d8360395e011319a8284cf5753ba22aa0fee7b
Melena Belt <58674385+melenabelt@users.noreply.github.com>
2023-10-04 16:20:58 -0500

    Add eslint setup (#6)
    Co-authored-by: melenabelt

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": "plugin:cypress/recommended",
+    "plugins": [
+        "@typescript-eslint",
+        "cypress"
+      ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+        "quotes": ["error", "double"],
+        "cypress/no-assigning-return-values": "error",
+        "cypress/no-unnecessary-waiting": "error",
+        "cypress/assertion-before-screenshot": "warn",
+        "cypress/no-force": "warn",
+        "no-unused-vars": "warn",
+        "require-jsdoc": "warn",
+        "max-len": [ "error", { "code": 120 } ]
+    }
+}
