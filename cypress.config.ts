08bb8cb50a264ec45c3fe086500897ea399b0e01
melenabelt <ecorr@softserveinc.com>
2023-10-04 16:56:18 -0500

    github actions setup

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "cypress";
 
 module.exports = defineConfig({
   e2e: {
+    video: false,
+    screenshotOnRunFailure: false,
     setupNodeEvents(on, config) {},
     baseUrl: "https://www.demoblaze.com/index.html",
     defaultCommandTimeout: 20000,
