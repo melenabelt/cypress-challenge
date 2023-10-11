d09b285c9451c1a164de7809375bcf79be9dfb0e
melenabelt <ecorr@softserveinc.com>
2023-10-10 21:45:47 -0500

    a variable was created to obtain demoqa url

--- cypress/e2e/demoqaFormPage.cy.ts
+++ cypress/e2e/demoqaFormPage.cy.ts
@@ -2,10 +2,12 @@ import DemoQaFormPage from "../pages/demoqaFormPage";
 
 const demoQaFormPage = new DemoQaFormPage();
 
+const demoqaBaseUrl = Cypress.env("demoqaUrl")
+
 describe("Form", () => {
 
     beforeEach(() => {
-        cy.visit(Cypress.env("demoqaUrl") + "automation-practice-form");
+        cy.visit(demoqaBaseUrl + "automation-practice-form");
     });
 
     it("Validate form is filled succesfully", () => {
