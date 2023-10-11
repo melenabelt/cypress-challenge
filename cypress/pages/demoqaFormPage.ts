29fc7af497e7c62fd63e9647ff8343765b7154ef
melenabelt <ecorr@softserveinc.com>
2023-10-10 23:23:45 -0500

    Uploading a file complete test and page using cypress-file-upload

--- cypress/pages/demoqaFormPage.ts
+++ cypress/pages/demoqaFormPage.ts
@@ -1,5 +1,6 @@
 import { formLocators } from "../support/demoqaLocators";
 
+
 class DemoQaFormPage {
   constructor() {}
 
---
+++ cypress/pages/uploadFilePage.ts
@@ -0,0 +1,17 @@
+import { fileUploaderLocators } from "../support/fileUploaderLocators";
+
+class UploadFilePage{
+    constructor() {
+    }
+
+    validateFileIsUploadedSuccesfully(){
+        cy.get(fileUploaderLocators.chooseFileButton).attachFile("dataContact.json");
+        cy.get(fileUploaderLocators.uploadButton).click()
+        cy.get(fileUploaderLocators.uploadedFileContainer).invoke('text').then((text) => {
+            cy.log(text)
+        })
+    }
+
+}
+
+export default UploadFilePage;
