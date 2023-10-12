76e4aa7d6c5ba9c203ceaab4d1975a58a2f25174
Elena Correa <melenabelt@gmail.com>
2023-10-11 21:25:46 -0500

    retries were implemented in cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,14 @@
 import { defineConfig } from "cypress";
 
 module.exports = defineConfig({
+  "retries": {
+    // Configure retry attempts for `cypress run`
+    // Default is 0
+    "runMode": 3,
+    // Configure retry attempts for `cypress open`
+    // Default is 0
+    "openMode": 2
+  },
   e2e: {
     video: false,
     screenshotOnRunFailure: false,
@@ -12,10 +20,9 @@ module.exports = defineConfig({
       demoqaUrl: "https://demoqa.com/",
       fileUploaderUrl: "https://the-internet.herokuapp.com/",
       w3schoolUrl: "https://www.w3schools.com/",
-      "RETRIES": 3
     },
-    defaultCommandTimeout: 30000,
-    pageLoadTimeout: 30000,
+    defaultCommandTimeout: 40000,
+    pageLoadTimeout: 40000,
 
     reporter: "cypress-multi-reporters",
     reporterOptions: {
